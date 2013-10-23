70c41a235b9ce1db88aa53e35e18b2a0ca039e3f
Alexander Saprykin <itallium@yandex.ru>
2013-10-23 14:25:04 +0900

    Изменения Main.java от 23.10.2013. Лишние методы были перенесены в отдельные классы (HumanMove.java, Field.java, CheckGame.java). Добавлена работа с исключениями, которые возникали при вводе неверных данных.

--- src/ru/itallium/tictactoe/Main.java
+++ src/ru/itallium/tictactoe/Main.java
@@ -6,188 +6,86 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    //Создание игрового поля
-    public static char field[][] = {
-            { '+', '+', '+' },
-            { '+', '+', '+' },
-            { '+', '+', '+' }
-    };
-
-    public static int difficulty = 1;
+    public static int difficulty = 0;
 	
 	public static boolean isPlayerMove;
-
-    public static void HumanMove() throws IOException {
-
-        int y = -1, x = -1;
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        while(x < 0 || x > 2 || y < 0 || y > 2 || field[y][x] != '+') {
-            System.out.println("Введите строку (1..3):");
-            y = Integer.parseInt(reader.readLine()) - 1;
-            System.out.println("Введите столбец (1..3)");
-            x = Integer.parseInt(reader.readLine()) - 1;
-            if (x < 0 || x > 2 || y < 0 || y > 2) {
-                System.out.println("Вы попали за пределы игрового поля. Повторите ввод.");
-                PrintField();
-                continue;
-            }
-            if (field[y][x] != '+') {
-                System.out.println("Эта клетка уже занята. Повторите ввод.");
-                PrintField();
-                continue;
-            }
-        }
-        field[y][x] = 'X';
-
-    }
-
-    public static void PrintField() {
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(field[i][j]);
-            }
-            System.out.println();
-        }
-
-    }
-
-    public static boolean CanMove() {
-
-        int canMove = 0;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (field[i][j] == '+') canMove = 1;
-            }
-        }
-        if (canMove == 1) return true;
-        else return false;
-
-    }
-
-    public static char CheckGame() {
-
-        char winner = '+';
-        //Проверка победителя по строкам
-        for(int i = 0; i < 3; i++) {
-            if(field[i][0] == field[i][1] && field[i][1] == field[i][2] && field[i][0] != '+') {
-                winner = field[i][0];
-                break;
-            }
-        }
-
-        //Проверка победителя по столбцам
-        for(int i = 0; i < 3; i++) {
-            if(field[0][i] == field[1][i] && field[1][i] == field[2][i] && field[i][0] != '+') {
-                winner = field[0][i];
-                break;
-            }
-        }
-
-        //Проверка победителя по главной диагонали
-        if(field[0][0] == field[1][1] && field[1][1] == field[2][2]) {
-            winner = field[0][0];
-        }
-
-        //Проверка победителя по дополнительной диагонали
-        if(field[0][2] == field[1][1] && field[1][1] == field[2][0]) {
-            winner = field[0][2];
-        }
-        return winner;
-
-    }
-	
-	public static void toggleTurn() {
-
-		isPlayerMove = isPlayerMove ? false : true;
-
-	}
-	
-	public static void doMove() throws IOException {
-
-		if (isPlayerMove) {
-            PrintField();
-			HumanMove();
-		} else {
-			CompMove.move();
-		}
-		toggleTurn();
-
-	}
-	
-	public static void clearField() {
-
-		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < 3; j++) {
-				field[i][j] = '+';
-			}
-		}
-
-	}
 
     public static void main(String[] args) throws IOException {
 
         int gameCount = 0;
 
-        System.out.println("Выберите сложность:\n1. Random-bot\n2. Умный соперник.");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        difficulty = Integer.parseInt(reader.readLine());
-		while(difficulty < 1 || difficulty > 2) {
+		do {
+            System.out.println("Select difficulty:\n1. Random-bot\n2. Genius.");
+            try {
+                difficulty = Integer.parseInt(reader.readLine());
+            } catch (Exception NumberFormatException) {
+                System.out.println("Incorrected input. Please enter only numbers.");
+                continue;
+            }
 			switch (difficulty) {
 			case 1:
-				System.out.println("Вы выбрали в соперники \"Random-bot\".");
+				System.out.println("Your select is: \"Random-bot\".");
 				break;
 			case 2:
-				System.out.println("\bВы выбрали в соперники \"Умный соперник\".");
+				System.out.println("\bYour select is: \"Умный соперник\".");
 				break;
-			default:
-				System.out.println("Вы ввели некорректные данные. Пожалуйста, повторите ввод.");
-				System.out.println("Выберите сложность:\n1. Random-bot\n2. Умный соперник.");
-				reader = new BufferedReader(new InputStreamReader(System.in));
-				difficulty = Integer.parseInt(reader.readLine());
-			}
-		}
+            default:
+                System.out.println("Input is not correct. Please retry input.");
+            }
+		} while(difficulty < 1 || difficulty > 2);
 
-		reader = new BufferedReader(new InputStreamReader(System.in));
-		
         do {
-
 			gameCount++;
-			clearField();
-            System.out.println("Игра №" + gameCount + " началась!");
+			Field.clearField();
+            System.out.println("Game №" + gameCount + " is started!");
 			
             float gamer = (float) Math.random();
             if (gamer <= 0.5) {
 				isPlayerMove = false;
-                System.out.println("Первым ходит компьютер.");
+                System.out.println("Computer is first.");
 			} else {
 				isPlayerMove = true;
-                System.out.println("Первым ходит игрок.");
+                System.out.println("Gamer is first.");
 			}
 			
-			while(CheckGame() == '+' && CanMove()) {
+			while(CheckGame.check() == Field.DEFAULT_SYMBOL && CheckGame.CanMove()) {
 				doMove();
 			}
 			
-            PrintField();
-            if (!CanMove()) {
-                System.out.println("Ходов больше нет.");
+            Field.PrintField();
+            if (!CheckGame.CanMove()) {
+                System.out.println("Field is full.");
             }
 			
-            if (CheckGame() == 'X') {
-				System.out.println("Поздравляю, ты выиграл!");
+            if (CheckGame.check() == Field.HUMAN_SYMBOL) {
+				System.out.println("Congratulation, you win!");
 			}
 
-            if (CheckGame() == '0') {
-				System.out.println("Ты проиграл.");
+            if (CheckGame.check() == Field.COMPUTER_SYMBOL) {
+				System.out.println("You lose.");
 			}
 			
-            System.out.print("\n~~~~~~~~\nХотите попробовать ещё раз? y - yes\n");
+            System.out.print("\n~~~~~~~~\nDo you retry? y - yes\n");
         } while(reader.readLine().equals("y"));
 		
-        System.out.println("Спасибо, что протестировали мою игру. Надеюсь вам понравилось.");
+        System.out.println("Thank you for testing my game. May you like.");
         System.out.print("Created by Itallium. 2013");
 
+    }
+
+    public static void doMove(){
+        if (isPlayerMove) {
+            Field.PrintField();
+            HumanMove.move();
+        } else {
+            CompMove.move();
+        }
+        toggleTurn();
+    }
+
+    public static void toggleTurn() {
+        isPlayerMove = isPlayerMove ? false : true;
     }
 
 }
